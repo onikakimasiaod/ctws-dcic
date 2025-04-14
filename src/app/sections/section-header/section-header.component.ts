6b99408a3f8691a2e4a01e921bc7349503f573bb
Kim Asiao <kim.asiao@w2m.com>
2025-04-14 18:57:46 +0200

    feat: section header

---
+++ src/app/sections/section-header/section-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './section-header.component.html',
+  styleUrl: './section-header.component.scss'
+})
+export class SectionHeaderComponent {
+
+}
