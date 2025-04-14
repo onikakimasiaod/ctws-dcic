f037cd03fe5944254ae371a36155aea0b511e60b
Kim Asiao <kim.asiao@w2m.com>
2025-04-14 19:41:30 +0200

    feat: migration ng 19

--- src/app/sections/section-header/section-header.component.ts
+++ src/app/sections/section-header/section-header.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-section-header',
-  standalone: true,
-  imports: [],
   templateUrl: './section-header.component.html',
   styleUrl: './section-header.component.scss'
 })
